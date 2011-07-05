3d1ab676ab344a663f6468dfb3e9914fa1797c4d
Lee Jones <scribblethink@gmail.com>
2011-07-05 19:09:52 -0400

    Add volume to vendor

---
+++ vendor/plugins/volume/volume.rb
@@ -0,0 +1,55 @@
+class Volume < Sinatra::Base
+  dir = File.dirname(File.expand_path(__FILE__))
+
+  set :views,  "#{dir}/server/views"
+  set :public, "#{dir}/server/public"
+  set :static, true
+
+  get '/' do
+    @current_volume = current_volume.to_s
+    erb :index
+  end
+
+  post '/volume' do
+    set_volume params[:volume]
+    redirect '/' unless request.xhr?
+  end
+
+  get '/volume.json' do
+    if callback = params[:callback]
+      content_type :js
+      "#{params[:callback]}(#{{:volume => current_volume}.to_json})"
+    else
+      content_type :json
+      {:volume => current_volume}.to_json
+    end
+  end
+  
+  post '/volume.json' do
+    `growlnotify --message '#{params[:volume]}'`
+    set_volume(params[:volume])
+    if callback = params[:callback]
+      content_type :js
+      "#{params[:callback]}(#{{:volume => current_volume}.to_json})"
+    else
+      content_type :json
+      {:volume => current_volume}.to_json
+    end
+  end
+  
+  def set_volume(value)
+    `osascript -e 'set volume output volume #{value}'`
+  end
+  
+  def current_volume
+    if volume_muted?
+      0
+    else
+      `osascript -e 'output volume of (get volume settings)'`.chomp.to_i
+    end
+  end
+  
+  def volume_muted?
+    `osascript -e 'output muted of (get volume settings)'`.chomp == 'true'
+  end
+end
