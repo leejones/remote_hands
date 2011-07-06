c41c1d6fea63e4c43e6011e7683773248083a237
Lee Jones <scribblethink@gmail.com>
2011-07-05 20:38:03 -0400

    Move Sinatra config to remotehands app

--- remotehands.rb
+++ remotehands.rb
@@ -1,4 +1,10 @@
 class Remotehands < Sinatra::Base
+  dir = File.dirname(File.expand_path(__FILE__))
+
+  set :views,  "#{dir}/views"
+  set :public, "#{dir}/public"
+  set :static, true
+
   get '/' do
     erb :index
   end
