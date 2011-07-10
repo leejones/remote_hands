4d4b396471ad7360f365c4bf46db7937faef2cc9
Lee Jones <scribblethink@gmail.com>
2011-07-09 21:30:45 -0400

    Remove views from vendored volume

--- vendor/plugins/volume/volume.rb
+++ vendor/plugins/volume/volume.rb
@@ -1,20 +1,4 @@
 class Volume < Sinatra::Base
-  dir = File.dirname(File.expand_path(__FILE__))
-
-  set :views,  "#{dir}/views"
-  set :public, "#{dir}/public"
-  set :static, true
-
-  get '/' do
-    @current_volume = current_volume.to_s
-    erb :index
-  end
-
-  post '/volume' do
-    set_volume params[:volume]
-    redirect '/' unless request.xhr?
-  end
-
   get '/volume.json' do
     if callback = params[:callback]
       content_type :js
