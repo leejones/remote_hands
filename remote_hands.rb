bbad44785797fcdcd9a2fa5d2de03ade3249653a
Lee Jones <scribblethink@gmail.com>
2011-07-09 21:33:30 -0400

    Move volume to app dir

--- remote_hands.rb
+++ remote_hands.rb
@@ -3,8 +3,8 @@ require 'bundler'
 
 Bundler.require
 
-require 'vendor/plugins/volume/volume'
 require 'vendor/plugins/say/say'
 require 'lib/itunes'
 
 require 'app/remote_hands'
+require 'app/volume'
