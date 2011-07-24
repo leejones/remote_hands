ca4cb5f3bd5917433d5fd88b3e33105ae9ccdb65
Lee Jones <scribblethink@gmail.com>
2011-07-23 22:39:47 -0400

    Use applescript path for itunes

--- lib/itunes.rb
+++ lib/itunes.rb
@@ -1,12 +1,12 @@
 module  Itunes
   module Helper
     def itunes_running?
-      `osascript applescripts/is_app_running.scpt iTunes`.chomp == 'true'
+      `osascript #{APPLESCRIPTS_PATH}/is_app_running.scpt iTunes`.chomp == 'true'
     end
     
     def get_itunes_volume
       if itunes_running?
-        `osascript applescripts/get_itunes_volume.scpt`.chomp.to_i
+        `osascript #{APPLESCRIPTS_PATH}/get_itunes_volume.scpt`.chomp.to_i
       else
         nil
       end
@@ -14,7 +14,7 @@ module  Itunes
     
     def set_itunes_volume(volume)
       if itunes_running?
-        `osascript applescripts/set_itunes_volume.scpt #{volume}`
+        `osascript #{APPLESCRIPTS_PATH}/set_itunes_volume.scpt #{volume}`
       else
         false
       end
